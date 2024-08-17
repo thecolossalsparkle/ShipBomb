539e41d45d6e809021fe0ef1905a468b397505e4
namratha2914 <123230208+namratha2914@users.noreply.github.com>
2024-08-17 20:32:25 +0530

    Update MultiPlayerRules.java

--- src/main/java/MultiPlayerRules.java
+++ src/main/java/MultiPlayerRules.java
@@ -2,9 +2,9 @@ public class MultiPlayerRules {
     void displayRules() {
         System.out.println("Here are the rules of this game : ");
         System.out.println("Multiplayer : ");
-        System.out.println("1. You will be Given with a 5 x 5 grid");
+        System.out.println("1. You will be Given with a 10 x 10 grid");
         System.out.println("2. 3 Ships 1x3 or 3x1 are hidden in the gird");
-        System.out.println("3.Player 1 has to set ships for Player 2 and vice versa");
+        System.out.println("3.Player 1 has to hides ships for Player 2 and vice versa");
         System.out.println("4. You don't have any limited chances but whoever takes down all 3 ships wins immediately");
         System.out.println(" ");
         System.out.println(" ");
